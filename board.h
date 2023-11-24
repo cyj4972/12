08a36dc8ee86fc92556f005062b4643473d3342e
cyj4972 <144991384+cyj4972@users.noreply.github.com>
2023-11-25 00:09:47 +0900

    Create board.h

---
+++ board.h
@@ -0,0 +1,15 @@
+#define N_BOARD				15
+#define N_COINPOS			12
+#define MAX_COIN			4
+#define MAX_DIE				6
+#define BOARDSTATUS_OK		1
+#define BOARDSTATUS_NOK		0
+
+int board_initBoard(void);
+int board_printBoardStatus(void);
+
+int board_getBoardStatus(int pos);
+int board_getBoardCoin(int pos);
+
+int board_getSharkPosition(void);
+int board_stepShark(void);
